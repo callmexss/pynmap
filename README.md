8326a3bb20bcfc8dbb4706188b420cb59347a16b
callmexss <callmexss@126.com>
2020-04-24 20:02:50 +0800

    v0.0.1

--- README.md
+++ README.md
@@ -1,13 +1,23 @@
 # pynmap
 
+- [pynmap](#pynmap)
+  - [需求](#%e9%9c%80%e6%b1%82)
+  - [原理](#%e5%8e%9f%e7%90%86)
+    - [TCP 三次握手](#tcp-%e4%b8%89%e6%ac%a1%e6%8f%a1%e6%89%8b)
+  - [实现](#%e5%ae%9e%e7%8e%b0)
+  - [复盘](#%e5%a4%8d%e7%9b%98)
+    - [多线程等待所有线程函数结束](#%e5%a4%9a%e7%ba%bf%e7%a8%8b%e7%ad%89%e5%be%85%e6%89%80%e6%9c%89%e7%ba%bf%e7%a8%8b%e5%87%bd%e6%95%b0%e7%bb%93%e6%9d%9f)
+    - [ICMP Ping 遇到一个问题](#icmp-ping-%e9%81%87%e5%88%b0%e4%b8%80%e4%b8%aa%e9%97%ae%e9%a2%98)
+  - [参考](#%e5%8f%82%e8%80%83)
+
 基于 Scapy 实现的仿 nmap SYN 扫描器
 
 ## 需求
 
-1. 基于 Scapy 包实现
+1. 基于 Scapy 实现
 2. 利用 SYN 扫描开放端口
 3. 多线程
-4. 全端口扫描
+4. 1-65535 全端口扫描
 5. 结果采用类似 nmap 的方式展示
 
 ```sh
@@ -34,6 +44,84 @@ PORT    STATE  SERVICE
 443/tcp open   https
 ```
 
+## 原理
+
+### TCP 三次握手
+
+TCP 三次握手几乎是程序员面试时一定会被问到的问题，握手的目的是为了建立连接
+
+
 ## 实现
 
-有命令行接口，为了方便采用 click 这个包。
+有命令行接口，为了方便使用 [click](https://click.palletsprojects.com/en/7.x/)。
+
+分析一下命令行的参数
+
+```sh
+--verbose 用于控制显示粒度
+--port 用于选择端口范围
+--timeout 控制每个数据包的等待时间
+--size 控制一个线程发送的数据包数目
+dst 是要扫描的 IP 地址
+```
+
+函数原型代码如下：
+
+```python
+@click.command()
+@click.option("--verbose", help="Increase verbosity level", required=False, default=False)
+@click.option("--port", help="Port ranges", default="1-65535")
+@click.option("--size", help="Thread group size", default=1000)
+@click.option("--timeout",
+              help="how much time to wait after the last packet has been sent",
+              default=3)
+@click.argument("dst", required=True, type=str)
+def pynmap(verbose, timeout, dst, port, size):
+    """A simple SYN scanner."""
+    pass
+```
+
+
+## 复盘
+
+### 多线程等待所有线程函数结束
+
+最开始的时候忘记了，写了这样的代码：
+
+```python
+th_li = [th1, th2, ...]  # several threads
+for th in th_li:
+    th.start()
+    th.join()
+```
+
+这就导致每个线程都等待上一个线程结束了才会开始，跟一个线程再跑没有区别，要等待子线程结束应该先全部 `start` 之后再 `join`。
+
+```python
+th_li = [th1, th2, ...]  # several threads
+
+[th.start() for th in th_li]
+[th.join() for th in th_li]
+```
+
+### ICMP Ping 遇到一个问题
+
+官方文档的写法是：
+
+```python
+>>> ans, unans = sr(IP(dst="192.168.1.1-254")/ICMP())
+```
+
+但是按照这种写法一直收不到响应的包，搜索以后发现需要给 ICMP 添加一个 id 参数就好了
+
+```python
+>>> ans, unans = sr(IP(dst="192.168.1.1-254")/ICMP(id=100))
+```
+
+
+
+## 参考
+
+1. [python multithreading wait till all threads finished](https://stackoverflow.com/questions/11968689/python-multithreading-wait-till-all-threads-finished)
+
+2. [Example of sending ICMP is not working #1490](https://github.com/secdev/scapy/issues/1490#issuecomment-416294800)
--- pynmap.py
+++ pynmap.py
@@ -1,23 +1,22 @@
-import os
 import time
-import logging
 import threading
 
 
 import click
-from scapy.all import sr1, IP, TCP
-
-
-# logging.basicConfig(level=logging.DEBUG,
-#                     filename="pynmap.log",
-#                     format='%(asctime)s : %(levelname)s : %(filename)s : '
-#                            '%(lineno)s : %(funcName)s : %(message)s')
+from scapy.all import sr, sr1, IP, ICMP, TCP, RandShort
 
 
 OPEN_PORTS = []
-timeout = 3
+timeout = 1.5
+size = 10000
 start_info = "Starting pynmap 0.01"
 locker = threading.Lock()
+m = {
+    "22": "ssh",
+    "53": "dns",
+    "80": "http",
+    "443": "https",
+}
 
 
 def show_info(info, extra=""):
@@ -38,10 +37,12 @@ def scan_batch(ip, ports, timeout=timeout, verbose=False):
                                       args=(ip, port, timeout, verbose)))
     for th in th_li:
         th.start()
+
+    for th in th_li:
         th.join()
 
 
-def scan(ip, port, timeout=timeout, verbose=False):
+def scan(ip, ports, timeout=timeout, verbose=False):
     """Do SYN scan on a specific ip and port
 
     Arguments:
@@ -50,51 +51,63 @@ def scan(ip, port, timeout=timeout, verbose=False):
 
     Keyword Arguments:
         timeout {int} -- how much time to wait after the last packet has been sent (default: {3})
-    """    
+    """
     # show_info(f"Scan {ip} {port}")
     global OPEN_PORTS
-    ret = sr1(IP(dst=ip)/TCP(dport=int(port), flags="S"),
-              timeout=timeout,
-              verbose=0)
+    ans, _ = sr(IP(dst=ip)/TCP(sport=RandShort(), dport=ports, flags="S"),
+                    timeout=timeout,
+                    verbose=0)
 
-    if verbose and ret:
-        show_discovery(port, ip)
-        OPEN_PORTS.append(port)
+    if verbose and ans:
+        for port in [x[1][TCP].sport for x in ans]:
+            show_discovery(port, ip)
 
-    if ret:
+    if ans:
         locker.acquire()
-        OPEN_PORTS.append(ret)
+        OPEN_PORTS.extend([x[1][TCP].sport for x in ans])
         locker.release()
 
-    return True if ret else False
+    return True if ans else False
 
     
-def scan_range(dst, start, end, timeout=timeout, verbose=False):
-    port_li = [[] for x in range(os.cpu_count())]  # create several thread lists
-    for port in range(start, end + 1):
-        i = port % os.cpu_count()
-        port_li[i].append(port)
+def scan_range(dst, start, end, timeout=timeout, verbose=False, size=size):
+    if start == end:
+        scan(dst, start, timeout, verbose)
+        return
 
     th_li = []
-    for each in port_li:
-        th_li.append(threading.Thread(target=scan_batch,
-                                      args=(dst, each, timeout, verbose)))
+    for i in range(start, end + 1, size):
+        if i + size > 65535:
+            ports = list(range(i, 65536))
+        else:
+            ports = list(range(i, i + size))
+        th_li.append(threading.Thread(target=scan,
+                                      args=(dst, ports, timeout, verbose)))
 
-    for th in th_li:
-        th.start()
-    
+    [x.start() for x in th_li]
+    [x.join() for x in th_li]
+   
 
 @click.command()
-@click.option("--verbose", help="Increase verbosity level", required=False, default=False)
-@click.option("--port", help="Port ranges", default="1-65535")
+@click.option("--verbose", help="Verbose or not (default False)", type=bool,
+               required=False, default=False)
+@click.option("--port", help="Port ranges(default 1-65535)", default="1-65535")
+@click.option("--size", help="Thread group size", default=1000)
 @click.option("--timeout",
               help="how much time to wait after the last packet has been sent",
               default=3)
 @click.argument("dst", required=True, type=str)
-def pynmap(verbose, timeout, dst, port):
+def pynmap(verbose, timeout, dst, port, size):
     """A simple SYN scanner."""
     global OPEN_PORTS
     start_time = time.time()
+    ans, _ = sr(IP(dst=dst)/ICMP(id=RandShort()), verbose=0, retry=2, timeout=timeout)
+    if not ans:
+        elapsed = time.time() - start_time
+        print("Note: Host seems down.")
+        print(f"pynmap done: 1 IP address(0 hosts up) scanned in {round(elapsed)} seconds")
+        return
+        
     if verbose:
         show_info(start_info)
 
@@ -108,7 +121,7 @@ def pynmap(verbose, timeout, dst, port):
         if verbose:
             show_info(f"Scanning {dst}[{1} ports]")
 
-    scan_range(dst, start, end, verbose=verbose, timeout=timeout)
+    scan_range(dst, start, end, verbose=verbose, timeout=timeout, size=size)
     end_time = time.time()
     elapsed = end_time - start_time
     if verbose:
@@ -117,11 +130,14 @@ def pynmap(verbose, timeout, dst, port):
     
     print(f"pynmap scan report for {dst}")
     print(f"Not shown: {end - start + 1 - len(OPEN_PORTS)} filtered ports")
-    print(OPEN_PORTS)
     if OPEN_PORTS:
-        print("PORT\tSTATE\tSERVICE")
+        print("PORT\t\tSTATE\t\tSERVICE")
         for port in OPEN_PORTS:
-            print(f"{port}/tcp\topen\{m.get(str(port), 'UNKNOWN')}")
+            port_str = str(port) + "/tcp"
+            open_str = "open"
+            print(f"{port_str:<8s}\t{open_str:<8s}\t{m.get(str(port), 'UNKNOWN')}")
+    print()
+    print(f"pynmap done: 1 IP address(up) scanned in {round(elapsed)} seconds")
 
 
 if __name__ == "__main__":
